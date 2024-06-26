3a010645e88f58c63b709fdc7130450bce5776c6
PhoeniX <rpx03775@gmail.com>
2024-06-26 19:16:19 +0530

    added images of the movies

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pickle
+import requests
+
+
+def fetch_movie_image(id):
+    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=b83372fae972a9fcdb7da6903903fe6c&language=en.US'.format(id))
+    response=response.json()
+    print(response)
+    return "https://image.tmdb.org/t/p/w500/"+response['poster_path']
+
 
 movies=pickle.load(open('movies.pkl','rb'))
 similarity=pickle.load(open('similarity.pkl','rb'))
@@ -8,10 +17,11 @@ def recommend(movie):
     distances=similarity[movie_index]
     movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
     recommended_movies=[]
+    recommended_movies_image=[]
     for i in movies_list:
-        movie_id=i[0]
         recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+        recommended_movies_image.append(fetch_movie_image(movies.iloc[i[0]].id))
+    return recommended_movies,recommended_movies_image
 
 st.title('Movie Recommendation System')
 
@@ -24,5 +34,21 @@ selected_movie=st.selectbox(
 
 if(st.button('Recommend')):
     st.write('Great!, Now you should watch the following movies:')
-    for i in recommend(selected_movie):
-        st.write(i)
+    movie_name,movie_image=recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+       st.text(movie_name[0])
+       st.image(movie_image[0])
+    with col2:
+       st.text(movie_name[1])
+       st.image(movie_image[1])
+    with col3:
+       st.text(movie_name[2])
+       st.image(movie_image[2])
+    with col4:
+       st.text(movie_name[3])
+       st.image(movie_image[3])
+    with col5:
+       st.text(movie_name[4])
+       st.image(movie_image[4])
+    
